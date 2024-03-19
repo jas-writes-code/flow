5eceffb693e0400b0baba286d6bc074051fbac4d
jas <jas@virgildoesthings.com>
2024-03-19 13:23:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-# core game file
-
 import pygame
-import math
+import pyaudio
 import random
+import time
+import math
 import os
 import json
+pygame.init()
 
-print("it works!")
+width = random.randint(100, 1000)
+height = random.randint(100, 1000)
+screen = pygame.display.set_mode((width, height))
+
+_settings = json.load(open('settings.json'))
+frames = _settings['video']['fps']
+if frames == "high":
+    _frames = 60
+else:
+    _frames = 30
+
+while True:
+    pygame.clock.tick(_frames)
