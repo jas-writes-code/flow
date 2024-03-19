6530f06158c76b9b63475010c13509eb53c7494a
jas <jas@virgildoesthings.com>
2024-03-19 09:52:53 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,7 @@
 # you should only need to run this once
 
 echo 'Thank you for downloading FLOW.'
+wget https://github.com/jas-writes-code/flow/releases/lates/code.zip ~/flow.zip
+unzip ~/flow.zip -d ~/flow
+rm -r ~/flow.zip
+cd ~/flow
