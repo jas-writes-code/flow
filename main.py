cb15034f97f9cfc256abb56da63ff4d895586fed
jas <jas@virgildoesthings.com>
2024-03-19 14:45:54 +0100

    fix settings and main

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ import time
 import math
 import os
 import json
-pygame.init()
 
-width = random.randint(100, 1000)
-height = random.randint(100, 1000)
-screen = pygame.display.set_mode((width, height))
+pygame.init()
+screen = pygame.display.set_mode((1600, 900))
+pygame.display.set_caption("FlowGame")
+clock = pygame.time.Clock()
 
 _settings = json.load(open('settings.json'))
-frames = _settings['video']['fps']
-if frames == "high":
-    _frames = 60
+_frames = _settings['video']['fps']
+if _frames == "high":
+    frames = 60
 else:
-    _frames = 30
+    frames = 30
+size = 1
 
 while True:
-    pygame.clock.tick(_frames)
+    screen.fill('#94167f')
+    pygame.draw.circle(screen, '#f9ac53', (800, 450), size)
+    size += 1
+    pygame.display.update()
+    clock.tick(frames)
