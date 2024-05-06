474a88da0469fd897399b780bd283ca70c27be8d
jas <jas@virgildoesthings.com>
2024-05-06 11:15:17 +0200

    tweaks to state switch

--- player/player.py
+++ player/player.py
@@ -14,6 +14,7 @@ running = physics.DoPhysics('player/placeholder', 100, 100, 400, 600, 0.3, 0.9, 
 running.load('player/placeholder')
 swinging = physics.DoPhysics('player/swinging', 128, 100, 400, 600, 0.3, 0.9, 0)
 player = idle
+keys = 0
 
 def setState(state, player):
     states = [idle, running, swinging]
@@ -36,7 +37,7 @@ def findHook():
             return element.get_x
 
 def spawn():
-    global player
+    global player, keys
     key = pygame.key.get_pressed() # controls
     if key[pygame.K_a] or key[pygame.K_LEFT]:
         physics.DoPhysics.move(player, - vars.speed)
@@ -47,22 +48,28 @@ def spawn():
     if key[pygame.K_w] or key[pygame.K_SPACE] or key[pygame.K_UP]:
         physics.DoPhysics.doJump(player)
         player = setState(1, player)
-    if ((key[pygame.K_d] or key[pygame.K_RIGHT]) or player.vel_x > 0) and player.cur_x > 725:
-        if abs(viewport.BGSPEED) <= 99:
-            viewport.BGSPEED += 0.5
     if key[pygame.K_q]:
         player = setState(2, player)
         player.vel_x, player.vel_y = movements.swing(player.cur_x, player.cur_y, player.vel_x, player.vel_y, findHook())
     if key[pygame.K_e]:
         player = setState(2, player)
         player.vel_x, player.vel_y = movements.pull(player.cur_x, player.cur_y, player.vel_x, player.vel_y, findHook())
+    if ((key[pygame.K_d] or key[pygame.K_RIGHT]) or player.vel_x > 0) and player.cur_x > 725:
+        if abs(viewport.BGSPEED) <= 99:
+            viewport.BGSPEED += 0.5
     else:
         viewport.BGSPEED *= 0.95
 
     for event in pygame.event.get(): # reset back to idle state when you release a key
+        if event.type == pygame.KEYDOWN:
+            keys += 1
         if event.type == pygame.KEYUP:
             if event.key in [pygame.K_a, pygame.K_LEFT, pygame.K_d, pygame.K_RIGHT, pygame.K_w, pygame.K_SPACE, pygame.K_UP, pygame.K_q, pygame.K_e]:
-                player = setState(0, player)
+                if keys != 1:
+                    keys -= 1
+                else:
+                    player = setState(0, player)
+                    keys -= 1
         if event.type == pygame.QUIT:
             vars.gameState = -1
 
